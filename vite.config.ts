e10916b09d04e7f0577016b367b7a4fa6f92ed35
DEVGIT-farhan <farhanfarhan.in@gmail.com>
2026-08-10 21:20:13 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env["GITHUB_ACTIONS"] === "true" ? "/one-path/" : "/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
